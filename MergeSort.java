67d8948eef670a4973acb0145f94bc554d0e1a55
rsmciftci <rsmciftci@gmail.com>
2023-04-18 23:05:24 +0300

    Worst case time complexity added

--- MergeSort.java
+++ MergeSort.java
@@ -1,3 +1,7 @@
+// Merge sort is defined as a sorting algorithm that works by dividing an array into smaller subarrays, 
+// sorting each subarray, and then merging the sorted subarrays back together to form the final sorted array.
+// O(n.log(n))
+
 class MergeSort{
 
     void merge(int arr[], int l, int m, int r)
