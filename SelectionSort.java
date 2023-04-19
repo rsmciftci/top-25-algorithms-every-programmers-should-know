9bbe0a15f0a5cd08e030b73101f1d499a4714a71
rsmciftci <rsmciftci@gmail.com>
2023-04-19 11:47:39 +0300

    Selection Sort

---
+++ SelectionSort.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+class SelectionSort {
+
+    // https://www.techiedelight.com/selection-sort-iterative-recursive/
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void selectionSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if(arr[j] < arr[min]) {
+                    min = j;
+
+                }
+
+            }
+            swap(arr, i, min);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 5, 8, 4, 1, 9, -2 };
+ 
+        selectionSort(arr);
+ 
+        // print the sorted array
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
