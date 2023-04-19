ade3d6ece7d5816698d3b5efeb93eebc7d2f07de
rsmciftci <rsmciftci@gmail.com>
2023-04-19 18:23:09 +0300

    unused variable removed

--- sorting/HeapSort.java
+++ sorting/HeapSort.java
@@ -63,8 +63,8 @@ class HeapSort{
 
 
     public static void main(String[] args) {
+        
         int[] arr  = { 12, 11, 13, 5, 6, 7 };
-        int N = arr.length;
 
         HeapSort ob = new HeapSort();
         ob.sort(arr);
